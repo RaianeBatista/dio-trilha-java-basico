1fc7e7867321b7b138e1b96493e9df05cb2793e4
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-03-29 20:31:00 -0300

    feat: add classe Scanner

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main (String[] args) throws Exception{
-        System.out.println("Hello World!");
+        Scanner teclado = new Scanner(System.in);
+       //TODO: conhecer e importar a classe Scanner
+        int numero;
+        String agencia, nomeCliente;
+        float saldo;
+
+       //Exibir as mensagens para o nosso usuário
+       //Obter pela classe scanner os valores digitados no terminal
+        System.out.println("Por favor digite o número da Agência: " );
+        agencia = teclado.nextLine();
+
+        System.out.println("Por favor digite o número da Conta:");
+        numero = Integer.parseInt(teclado.nextLine());
+
+        System.out.println("Por favor digite o nome do CLiente:");
+        nomeCliente = teclado.nextLine();
+
+        System.out.println("Por favor digite o saldo inicial:");
+        saldo = Float.parseFloat(teclado.nextLine());
+
+        
+
+       //Exibir a mensagem da conta criada
+       System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+       teclado.close();
     }
 }
